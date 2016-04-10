44eaa7331cf53b55b126d8058d55512ec7e39d2a
Roland-Schneider <Roland-Schneider@users.noreply.github.com>
2016-04-10 21:36:08 +0200

    Production scripts for AppVeyor build

--- ProductionTools/AppVeyor/build.cmd
+++ ProductionTools/AppVeyor/build.cmd
@@ -1,7 +1,18 @@
 @setlocal
+
+@echo Platform=%PLATFORM%
+@echo Configuration=%CONFIGURATION%
+
 msbuild StaMa.sln /t:Clean,Build /p:Configuration=Debug /p:Platform="Any CPU"
 msbuild StaMaNETMF.sln /t:Clean,Build /p:Configuration=Debug /p:Platform="Any CPU"
 msbuild DevelopersGuide\StaMaDevelopersGuide.shfbproj /t:Clean,Build /p:Configuration=Debug /p:Platform="AnyCPU"
+
+:: Generating nuget package
+nuget pack StaMa.StateMachine.nuspec -Symbols -OutputDirectory bin\
+appveyor PushArtifact "bin\*.nupkg"
+
+:: Generating github release package
+set StaMaZip=bin\StaMa_State_Machine_Controller_Library.%APPVEYOR_BUILD_VERSION%.zip
 
 xcopy /S /F /Y ProductionTools\ExtractVisioPages\Artifacts\*.vs* .
 del StaMa\StaMa.snk
@@ -10,20 +21,19 @@ for /f %%i in ('dir /b /s /a:d DOTNETMF_FS_EMULATION') do rd /s /q %%i
 for /f %%i in ('dir /b /s OnBoardFlash.*') do del %%i
 
 @pushd ..
-set StaMaZip=StaMa\bin\StaMa_State_Machine_Controller_Library.zip
-7z a %StaMaZip% StaMa\StaMa
-7z a %StaMaZip% StaMa\Tests
-7z a %StaMaZip% StaMa\Samples
-7z a %StaMaZip% StaMa\bin\netmf\AnyCPU
-7z a %StaMaZip% StaMa\bin\net-4.0\AnyCPU
-7z a %StaMaZip% StaMa\bin\netmf\DevelopersGuide\StaMaDevelopersGuide.chm
-7z a %StaMaZip% StaMa\ChangeLog.txt
-7z a %StaMaZip% StaMa\LICENSE
-7z a %StaMaZip% StaMa\StaMa.sln
-7z a %StaMaZip% StaMa\StaMaNETMF.sln
-7z a %StaMaZip% StaMa\StaMaShapes.vst
+7z a StaMa\%StaMaZip% StaMa\StaMa
+7z a StaMa\%StaMaZip% StaMa\Tests
+7z a StaMa\%StaMaZip% StaMa\Samples
+7z a StaMa\%StaMaZip% StaMa\bin\netmf\AnyCPU
+7z a StaMa\%StaMaZip% StaMa\bin\net-4.0\AnyCPU
+7z a StaMa\%StaMaZip% StaMa\bin\netmf\DevelopersGuide\StaMaDevelopersGuide.chm
+7z a StaMa\%StaMaZip% StaMa\ChangeLog.txt
+7z a StaMa\%StaMaZip% StaMa\LICENSE
+7z a StaMa\%StaMaZip% StaMa\StaMa.sln
+7z a StaMa\%StaMaZip% StaMa\StaMaNETMF.sln
+7z a StaMa\%StaMaZip% StaMa\StaMaShapes.vst
 @popd
 
-nuget pack StaMa.StateMachine.nuspec -Symbols -OutputDirectory bin\
+appveyor PushArtifact "%StaMaZip%"
 
 @endlocal
